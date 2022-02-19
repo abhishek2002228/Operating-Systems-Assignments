79bf8198148fbafad5340618655e0af3dce8c0d9
Sukrit <ksukrit2001@gmail.com>
2022-02-19 13:41:52 +0530

    Initial code

--- sudoku.c
+++ sudoku.c
@@ -4,10 +4,23 @@
 // Header files for threads
 #include <pthread.h>
 
+
+// Math
+#include <math.h>
+
 // Fork/ Proc related headers
 #include<unistd.h>
 #include<sys/wait.h>
+
+// Error handling
 #include<errno.h>
+#include <assert.h>
+
+// global variable to store minisquare size;
+int ms_size;
+
+// compile command
+// gcc -pthread sudoku.c -o sudoku.out -lm
 
 
 void read_grid_from_file(int size, char *ip_file, int grid[36][36]) {
@@ -30,6 +43,40 @@ void print_grid(int size, int grid[36][36]) {
 	}
 }
 
+// checks if configuration is valid
+// needs to be modified for multi threading.
+int check(int board[36][36], int row, int col, int c, int s){
+
+    for(int i = 0; i < s; i++) {
+        if(board[i][col] != 0 && board[i][col] == c) return 0; //check row
+        if(board[row][i] != 0 && board[row][i] == c) return 0; //check column
+        if(board[ms_size * (row / ms_size) + i / ms_size][ ms_size * (col / ms_size) + i % ms_size] != 0 && 
+board[ms_size * (row / ms_size) + i / ms_size][ms_size * (col / ms_size) + i % ms_size] == c) return 0; //check 3*3 block
+    }
+    return 1;
+}
+
+int solve(int board[36][36], int s) {
+    for(int i = 0; i < s; i++){
+        for(int j = 0; j < s; j++){
+            if(board[i][j] == 0){
+                for(int c = 1; c <= s; c++){
+                    if(check(board, i, j, c,s) == 1){
+                        board[i][j] = c; 
+                        if(solve(board,s))
+                            return 1; 
+                        else
+                            board[i][j] = 0; 
+                    }
+                }
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
+
+
 int main(int argc, char *argv[]) {
 	int grid[36][36], size, i, j;
 	
@@ -39,9 +86,15 @@ int main(int argc, char *argv[]) {
 	}
 	
 	size = atoi(argv[1]);
+	ms_size = (int) sqrt(size);
+	printf(" mini sqaure size is %d \n",ms_size);
+	assert(size == 1 || size == 4 || size == 9 || size == 16 || size == 25 || size == 36);
 	read_grid_from_file(size, argv[2], grid);
 	
 	/* Do your thing here */
-	
+	print_grid(size,grid);
+	printf("Now solving \n");
+	solve(grid,size);
 	print_grid(size, grid);
+	return 0;
 }
